d456bb75a5373807d0699679558a9a68a34e4dfd
durgaprasadnaidu-dp <durgaprasadnaidu2167@gmail.com>
2026-04-14 12:13:57 +0530

    Initial commit

---
+++ build/precache-manifest.6011bdbe7df5c544596d1e3cd5a6499a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1417dc9317a11681e5d1ce8819cb09cd",
+    "url": "/FacialEmotionDetector/index.html"
+  },
+  {
+    "revision": "affec1e7f0a5a38a58bc",
+    "url": "/FacialEmotionDetector/static/css/main.b6389da0.chunk.css"
+  },
+  {
+    "revision": "7987367c23960af2d255",
+    "url": "/FacialEmotionDetector/static/js/2.63482c85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FacialEmotionDetector/static/js/2.63482c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "affec1e7f0a5a38a58bc",
+    "url": "/FacialEmotionDetector/static/js/main.08c552af.chunk.js"
+  },
+  {
+    "revision": "75a6fc85e790bef0b749",
+    "url": "/FacialEmotionDetector/static/js/runtime-main.005ee6e1.js"
+  },
+  {
+    "revision": "5cf585fb2a38b17195dae89817db5b7f",
+    "url": "/FacialEmotionDetector/static/media/hero-bg.5cf585fb.png"
+  }
+]);
